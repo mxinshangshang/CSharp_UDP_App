4896eff5800188ba001532e1310346d7ae3c5b67
mxin chiang <jiangmingxin825@live.cn>
2015-03-21 10:56:52 +0800

    CSharp UDP
    CSharp UDP

---
+++ UDP_App/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace UDP_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbLocalIP = new System.Windows.Forms.TextBox();
+            this.tbLocalPort = new System.Windows.Forms.TextBox();
+            this.tbRemoteIP = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
+            this.tbRemotePort = new System.Windows.Forms.TextBox();
+            this.tbMsg = new System.Windows.Forms.TextBox();
+            this.tbSendMsg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbLocalIP
+            // 
+            this.tbLocalIP.Location = new System.Drawing.Point(23, 12);
+            this.tbLocalIP.Name = "tbLocalIP";
+            this.tbLocalIP.Size = new System.Drawing.Size(153, 21);
+            this.tbLocalIP.TabIndex = 1;
+            // 
+            // tbLocalPort
+            // 
+            this.tbLocalPort.Location = new System.Drawing.Point(182, 12);
+            this.tbLocalPort.Name = "tbLocalPort";
+            this.tbLocalPort.Size = new System.Drawing.Size(61, 21);
+            this.tbLocalPort.TabIndex = 2;
+            // 
+            // tbRemoteIP
+            // 
+            this.tbRemoteIP.Location = new System.Drawing.Point(23, 49);
+            this.tbRemoteIP.Name = "tbRemoteIP";
+            this.tbRemoteIP.Size = new System.Drawing.Size(153, 21);
+            this.tbRemoteIP.TabIndex = 3;
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(264, 229);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(178, 58);
+            this.btSend.TabIndex = 4;
+            this.btSend.Text = "Send";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // tbRemotePort
+            // 
+            this.tbRemotePort.Location = new System.Drawing.Point(182, 49);
+            this.tbRemotePort.Name = "tbRemotePort";
+            this.tbRemotePort.Size = new System.Drawing.Size(61, 21);
+            this.tbRemotePort.TabIndex = 5;
+            // 
+            // tbMsg
+            // 
+            this.tbMsg.Location = new System.Drawing.Point(23, 87);
+            this.tbMsg.Multiline = true;
+            this.tbMsg.Name = "tbMsg";
+            this.tbMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbMsg.Size = new System.Drawing.Size(220, 200);
+            this.tbMsg.TabIndex = 6;
+            // 
+            // tbSendMsg
+            // 
+            this.tbSendMsg.Location = new System.Drawing.Point(264, 87);
+            this.tbSendMsg.Multiline = true;
+            this.tbSendMsg.Name = "tbSendMsg";
+            this.tbSendMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbSendMsg.Size = new System.Drawing.Size(178, 129);
+            this.tbSendMsg.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 299);
+            this.Controls.Add(this.tbSendMsg);
+            this.Controls.Add(this.tbMsg);
+            this.Controls.Add(this.tbRemotePort);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.tbRemoteIP);
+            this.Controls.Add(this.tbLocalPort);
+            this.Controls.Add(this.tbLocalIP);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "UDP_Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbLocalIP;
+        private System.Windows.Forms.TextBox tbLocalPort;
+        private System.Windows.Forms.TextBox tbRemoteIP;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.TextBox tbRemotePort;
+        private System.Windows.Forms.TextBox tbMsg;
+        private System.Windows.Forms.TextBox tbSendMsg;
+    }
+}
+
---
+++ UDP_App/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace UDP_App
+{
+    public partial class Form1 : Form
+    {
+        IPAddress ip;
+        int port;
+        private IPEndPoint ipLocalPoint;
+        private EndPoint RemotePoint;
+        private Socket mySocket;
+        private bool RunningFlag = false; 
+        public Form1()
+        {
+            InitializeComponent();
+            //tbLocalIP.Text = getIPAddress();
+        }
+
+        private string getIPAddress()
+        {
+            // 获得本机局域网IP地址  
+            //IPAddress[] AddressList = Dns.GetHostByName(Dns.GetHostName()).AddressList;
+            IPAddress[] AddressList = Dns.GetHostByName(Dns.GetHostName()).AddressList;
+            if (AddressList.Length < 1)
+            {
+                return "";
+            }
+            return AddressList[2].ToString();
+        }
+
+        private int getValidPort(string port)
+        {
+            int lport;
+            //测试端口号是否有效  
+            try
+            {
+                //是否为空  
+                if (port == "")
+                {
+                    throw new ArgumentException(
+                        "端口号无效，不能启动DUP");
+                }
+                lport = System.Convert.ToInt32(port);
+            }
+            catch (Exception e)
+            {
+                //ArgumentException,   
+                //FormatException,   
+                //OverflowException  
+                Console.WriteLine("无效的端口号：" + e.ToString());
+                this.tbMsg.AppendText("无效的端口号：" + e.ToString() + "\n");
+                return -1;
+            }
+            return lport;
+        }
+
+
+        private IPAddress getValidIP(string ip)
+        {
+            IPAddress lip = null;
+            //测试IP是否有效  
+            try
+            {
+                //是否为空  
+                if (!IPAddress.TryParse(ip, out lip))
+                {
+                    throw new ArgumentException(
+                        "IP无效，不能启动DUP");
+                }
+            }
+            catch (Exception e)
+            {
+                //ArgumentException,   
+                //FormatException,   
+                //OverflowException  
+                Console.WriteLine("无效的IP：" + e.ToString());
+                this.tbMsg.AppendText("无效的IP：" + e.ToString() + "\n");
+                return null;
+            }
+            return lip;
+        }
+
+        //得到本机IP，设置UDP端口号 
+    
+        public delegate void MyInvoke(string strRecv);
+        private void ReceiveHandle()
+        {
+            //接收数据处理线程  
+            string msg;
+            byte[] data = new byte[1024];
+            MyInvoke myI = new MyInvoke(UpdateMsgTextBox);
+            while (RunningFlag)
+            {
+
+                if (mySocket == null || mySocket.Available < 1)
+                {
+                    Thread.Sleep(200);
+                    continue;
+                }
+                //跨线程调用控件  
+                //接收UDP数据报，引用参数RemotePoint获得源地址  
+                int rlen = mySocket.ReceiveFrom(data, ref RemotePoint);
+                msg = Encoding.Default.GetString(data, 0, rlen);
+                tbMsg.BeginInvoke(myI, new object[] { RemotePoint.ToString() + " : " + msg });
+
+            }
+        }
+
+        private void UpdateMsgTextBox(string msg)
+        {
+            //接收数据显示  
+            this.tbMsg.AppendText(msg + "\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ip = getValidIP(tbLocalIP.Text);
+            port = getValidPort(tbLocalPort.Text);
+            ipLocalPoint = new IPEndPoint(ip, port);
+
+            //定义网络类型，数据连接类型和网络协议UDP  
+            mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //绑定网络地址  
+            mySocket.Bind(ipLocalPoint);
+
+            //得到客户机IP  
+            ip = getValidIP(tbRemoteIP.Text);
+            port = getValidPort(tbRemotePort.Text);
+            IPEndPoint ipep = new IPEndPoint(ip, port);
+            RemotePoint = (EndPoint)(ipep);
+
+            //启动一个新的线程，执行方法this.ReceiveHandle，  
+            //以便在一个独立的进程中执行数据接收的操作  
+
+            RunningFlag = true;
+            Thread thread = new Thread(new ThreadStart(this.ReceiveHandle));
+            thread.Start();  
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            string msg;
+            msg = tbSendMsg.Text;
+            //发送UDP数据包  
+            byte[] data = Encoding.Default.GetBytes(msg);
+            mySocket.SendTo(data, data.Length, SocketFlags.None, RemotePoint);
+        }
+    }
+}
